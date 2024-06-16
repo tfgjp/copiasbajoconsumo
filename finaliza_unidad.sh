72ff70d37eadca0317b0cacfc861b37989a6c3da
tfgjp <bonitainformatica@gmail.com>
2024-06-16 08:02:30 +0200

    Create finaliza_unidad.sh

---
+++ finaliza_unidad.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Parametrización:
+# La IP del dispositivo enchufe
+#IP_DISPOSITIVO="192.168.99.11"
+#UNIDADTRABAJO="/scripts/"
+
+IP_DISPOSITIVO="192.168.12.94"
+UNIDADTRABAJO="/scripts/proyecto"
+
+cd $UNIDADTRABAJO
+
+# Desmonta la unidad cifrada
+sudo umount /mnt/unidadProyecto/
+if [ $? -ne 0 ]; then
+    echo "Error al desmontar unidadProyecto"
+    exit 1
+else
+    echo "unidadProyecto desmontada con éxito"
+fi
+
+# Cierra el mapeo del dispositivo cifrado
+sudo cryptsetup close cifrada
+if [ $? -ne 0 ]; then
+    echo "Error al cerrar el dispositivo cifrado"
+    exit 1
+else
+    echo "Dispositivo cifrado cerrado con éxito"
+fi
+
+#Instrucción de apagado del enchufe smart
+./hs100/hs100.sh off -i $IP_DISPOSITIVO
